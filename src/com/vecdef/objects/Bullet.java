f31cb7fe4da0ffa6819341f63634cb129a612fae
Branden Monroe <brandencmonroe@gmail.com>
2015-10-12 13:01:17 -0500

    Fixed the bullet destroying itself on collision with window borders.

--- src/com/vecdef/objects/Bullet.java
+++ src/com/vecdef/objects/Bullet.java
@@ -1,12 +1,11 @@
 package com.vecdef.objects;
 
-import org.javatroid.core.Window;
 import org.javatroid.math.Vector2f;
 import org.javatroid.math.Vector3f;
 
 public class Bullet extends Entity{
 	
-	private static final float EXPIRY_PADDING = 450;
+	int timeActive = 0;
 	
 	public Bullet(Vector2f position, Vector2f velocity){
 	    transform.setTranslation(position);
@@ -27,15 +26,13 @@ public class Bullet extends Entity{
 	}
 	
 	public void onUpdate(Grid grid, float dt){
-		
+		timeActive++;
 		grid.applyExplosiveForce(velocity.length(), new Vector3f(transform.getTranslation().x, transform.getTranslation().y, 0.0F), 80.0F);
-
 	    if (velocity.lengthSquared() > 0.0F) {
 	      transform.setOrientation(velocity.direction());
 	    }
-	    
-	    if ((transform.getTranslation().x < -Window.getWidth() / 2 - EXPIRY_PADDING) || (transform.getTranslation().x > Window.getWidth() / 2 + EXPIRY_PADDING) || (transform.getTranslation().y < -Window.getHeight() / 2 - EXPIRY_PADDING) || (transform.getTranslation().y > Window.getHeight() / 2 + EXPIRY_PADDING))
-	      bExpired = true;
+	    if(timeActive > 400)
+	    	bExpired = true;
 	}
 	
 }
