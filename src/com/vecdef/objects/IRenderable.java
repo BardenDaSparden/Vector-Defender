ef01e51b5f28a4820b25da1cb09ba8a263a94828
Branden Monroe <brandencmonroe@gmail.com>
2015-10-12 13:46:09 -0500

    Removed unused method "isDrawn"

--- src/com/vecdef/objects/IRenderable.java
+++ src/com/vecdef/objects/IRenderable.java
@@ -1,27 +1,13 @@
 package com.vecdef.objects;
 
-import org.javatroid.math.Vector4f;
-
-import com.vecdef.model.DefaultMesh;
 import com.vecdef.model.Mesh;
 import com.vecdef.model.Transform2D;
 
 public interface IRenderable {
 	
-	default public Mesh getMesh(){
-		return new DefaultMesh();
-	}
-	
-	default public Transform2D getTransform(){
-		return new Transform2D();
-	}
-	
-	default public Vector4f getBaseColor(){
-		return new Vector4f(1, 1, 1, 1);
-	}
-	
-	default public float getOpacity(){
-		return 1;
-	}
+	public Mesh getMesh();
+	public Transform2D getTransform();
+	public float getOpacity();
+	public void setOpacity(float opacity);
 	
 }
