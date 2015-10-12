983093cf28608b12b48e537733a9bdbe93e9adab
Branden Monroe <brandencmonroe@gmail.com>
2015-10-12 12:32:17 -0500

    Disabled vsync.

--- src/com/vecdef/core/Main.java
+++ src/com/vecdef/core/Main.java
@@ -3,8 +3,10 @@ package com.vecdef.core;
 public class Main {
 
 	public static void main(String[] args){
-	
+		Application application = new VectorDefender();
 		ApplicationSettings settings = new ApplicationSettings();
+		ApplicationLauncher launcher = new ApplicationLauncher(application, settings);
+		
 		settings.width = 1600;
 		settings.height = 900;
 		settings.fullscreen = true;
@@ -13,12 +15,8 @@ public class Main {
 		settings.renderrate = 60;
 		settings.updaterate = 60;
 		settings.multisamples = 4;
-		
-		Application application = new VectorDefender();
-		
-		ApplicationLauncher launcher = new ApplicationLauncher(application, settings);
-		launcher.launch();
-		
+			
+		launcher.launch();		
 	}
 	
 }
