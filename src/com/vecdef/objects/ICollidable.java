dbb2d87cf3c3f551058cef19845b4cae2d97450a
torqu_000 <torqu_000@barden>
2015-10-14 01:32:02 -0500

    Added "ContactEventListener" removal method.

--- src/com/vecdef/objects/ICollidable.java
+++ src/com/vecdef/objects/ICollidable.java
@@ -6,7 +6,8 @@ public interface ICollidable {
 
 	public Transform2D getTransform();
 	public int getRadius();
-	public void addContactListener(ContactEventListener event);
+	public void addContactListener(ContactEventListener listener);
+	public void removeContactListener(ContactEventListener listener);
 	public void onContact(ContactEvent event);
 	public int getGroupMask();
 	public int getCollisionMask();
