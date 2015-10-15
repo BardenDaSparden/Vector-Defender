835354d60976fc4931e814c7ac55debec7bc0e5e
Branden Monroe <brandencmonroe@gmail.com>
2015-10-15 03:31:06 -0500

    Added BLACK_HOLE bitmask.

--- src/com/vecdef/objects/CollisionMasks.java
+++ src/com/vecdef/objects/CollisionMasks.java
@@ -5,4 +5,5 @@ public class CollisionMasks {
 	public final int BULLET			= 0b0000000000000010;
 	public final int ENEMY			= 0b0000000000000100;
 	public final int MULTIPLIER 	= 0b0000000000001000;
+	public final int BLACK_HOLE 	= 0b0000000000010000;
 }
