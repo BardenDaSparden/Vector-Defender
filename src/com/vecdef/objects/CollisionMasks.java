7e6e4f26284bbb9fea0fbfeac2ee4b224458ae83
Branden Monroe <brandencmonroe@gmail.com>
2015-10-18 00:37:46 -0500

    Removed static scope of each bitmask.

--- src/com/vecdef/objects/CollisionMasks.java
+++ src/com/vecdef/objects/CollisionMasks.java
@@ -1,11 +1,11 @@
 package com.vecdef.objects;
 
 public class CollisionMasks {
-	public static final int PLAYER 		= 0b0000000000000001;
-	public static final int BULLET			= 0b0000000000000010;
-	public static final int ENEMY			= 0b0000000000000100;
-	public static final int MULTIPLIER 	= 0b0000000000001000;
-	public static final int BLACK_HOLE 	= 0b0000000000010000;
+	public final int PLAYER 		= 0b0000000000000001;
+	public final int BULLET			= 0b0000000000000010;
+	public final int ENEMY			= 0b0000000000000100;
+	public final int MULTIPLIER 	= 0b0000000000001000;
+	public final int BLACK_HOLE 	= 0b0000000000010000;
 	
 	@Deprecated
 	public String asString(int mask){
