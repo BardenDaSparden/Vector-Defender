c163da3958e98d98795ff80e92347bdb989cd269
Branden Monroe <brandencmonroe@gmail.com>
2015-10-18 00:38:06 -0500

    Tweaked default mesh's size.

--- src/com/vecdef/model/DefaultMesh.java
+++ src/com/vecdef/model/DefaultMesh.java
@@ -7,12 +7,12 @@ public class DefaultMesh extends Mesh{
 
 	public DefaultMesh(){
 		LinePrimitive p = new LinePrimitive();
-		p.addVertex(new Vector2f(-5, -5), new Vector4f(1, 0, 1, 1));
-		p.addVertex(new Vector2f(-5, 5), new Vector4f(1, 0, 1, 1));
-		p.addVertex(new Vector2f(-5, 5), new Vector4f(1, 0, 1, 1));
+		p.addVertex(new Vector2f(-5, -3), new Vector4f(1, 0, 1, 1));
+		p.addVertex(new Vector2f(-5, 3), new Vector4f(1, 0, 1, 1));
+		p.addVertex(new Vector2f(-5, 3), new Vector4f(1, 0, 1, 1));
 		p.addVertex(new Vector2f(5, 0), new Vector4f(1, 1, 1, 1));
 		p.addVertex(new Vector2f(5, 0), new Vector4f(1, 1, 1, 1));
-		p.addVertex(new Vector2f(-5, -5), new Vector4f(1, 0, 1, 1));
+		p.addVertex(new Vector2f(-5, -3), new Vector4f(1, 0, 1, 1));
 		
 		
 		MeshLayer layer1 = new MeshLayer();
