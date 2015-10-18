8995f7c676045690c6d55ca21d5864d008fb67f2
Branden Monroe <brandencmonroe@gmail.com>
2015-10-18 01:37:34 -0500

    Improved movement.

--- src/com/vecdef/ai/ZoomerBehaviour.java
+++ src/com/vecdef/ai/ZoomerBehaviour.java
@@ -1,6 +1,5 @@
 package com.vecdef.ai;
 
-import org.javatroid.core.Window;
 import org.javatroid.math.FastMath;
 import org.javatroid.math.Vector2f;
 
@@ -10,33 +9,32 @@ import com.vecdef.objects.Entity;
 public class ZoomerBehaviour extends Behavior{
 
 	float speed = 7;
-	float direction = FastMath.random() * 360f;
-	boolean isVelocitySet = false;
+	int gridWidth;
+	int gridHeight;
 	
 	public ZoomerBehaviour(Scene scene){
 		super(scene);
+		gridWidth = scene.getGrid().getWidth();
+		gridHeight = scene.getGrid().getHeight();
 	}
 	
 	@Override
 	public void create(Entity self){
-		
+		float direction = FastMath.random() * 360f;
+		self.getVelocity().set(new Vector2f(FastMath.cosd(direction) * speed, FastMath.sind(direction) * speed));
+		self.setAngularVelocity(speed);
 	}
 	
 	@Override
 	public void update(Entity self) {
-		if(!isVelocitySet){
-			isVelocitySet = true;
-			self.getVelocity().set(new Vector2f(FastMath.cosd(direction) * speed, FastMath.sind(direction) * speed));
-			self.setAngularVelocity(speed);
-		}
 		
-		if ((self.getTransform().getTranslation().x < -Window.getWidth() / 2) || (self.getTransform().getTranslation().x > Window.getWidth() / 2)) {
-			self.getTransform().getTranslation().x = FastMath.clamp(-Window.getWidth() / 2 + 1, Window.getWidth() / 2 - 1, self.getTransform().getTranslation().x);
+		if ((self.getTransform().getTranslation().x < -gridWidth / 2) || (self.getTransform().getTranslation().x > gridWidth / 2)) {
+			self.getTransform().getTranslation().x = FastMath.clamp(-gridWidth / 2 + 1, gridWidth / 2 - 1, self.getTransform().getTranslation().x);
 			self.getVelocity().x *= -1.0F;
 	    }
 
-	    if ((self.getTransform().getTranslation().y < -Window.getHeight() / 2) || (self.getTransform().getTranslation().y > Window.getHeight() / 2)) {
-	    	self.getTransform().getTranslation().y = FastMath.clamp(-Window.getHeight() / 2 + 1, Window.getHeight() / 2 - 1, self.getTransform().getTranslation().y);
+	    if ((self.getTransform().getTranslation().y < -gridHeight / 2) || (self.getTransform().getTranslation().y > gridHeight / 2)) {
+	    	self.getTransform().getTranslation().y = FastMath.clamp(-gridHeight / 2 + 1, gridHeight / 2 - 1, self.getTransform().getTranslation().y);
 	    	self.getVelocity().y *= -1.0F;
 	    }
 		
@@ -44,7 +42,6 @@ public class ZoomerBehaviour extends Behavior{
 
 	@Override
 	public void destroy(Entity object) {
-		// TODO Auto-generated method stub
 		
 	}
 }
