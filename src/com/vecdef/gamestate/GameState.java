fcc12afda8a430e52b8394a53eba72e64294a5c4
Branden Monroe <brandencmonroe@gmail.com>
2015-10-29 05:20:51 -0500

    Added Input and Gamepad.

--- src/com/vecdef/gamestate/GameState.java
+++ src/com/vecdef/gamestate/GameState.java
@@ -2,14 +2,26 @@ package com.vecdef.gamestate;
 
 import java.util.ArrayList;
 
+import org.javatroid.core.Input;
+
+import com.toolkit.inputstate.Gamepad;
+
 public class GameState {
 
+	protected Input input;
+	protected Gamepad gamepad;
 	ArrayList<GState> allStates;
 	int idx;
 	
-	public GameState(){
+	public GameState(Input input, Gamepad gamepad){
+		this.input = input;
+		this.gamepad = gamepad;
 		allStates = new ArrayList<GState>();
 		idx = 0;
+	}
+	
+	public void initialize(){
+		allStates.get(idx).initialize();
 	}
 	
 	public void update(){
