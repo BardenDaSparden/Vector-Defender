3fc46d5ea51a639b43801a2dcecce231c1e32f42
Branden Monroe <brandencmonroe@gmail.com>
2015-10-29 05:23:40 -0500

    Re-implemented input handling with Gamestate.

--- src/com/vecdef/core/VectorDefender.java
+++ src/com/vecdef/core/VectorDefender.java
@@ -3,12 +3,15 @@ package com.vecdef.core;
 import org.javatroid.core.Input;
 import org.javatroid.core.Resources;
 
+import com.toolkit.inputstate.Gamepad;
 import com.vecdef.gamestate.GameState;
 import com.vecdef.gamestate.MenuState;
 import com.vecdef.gamestate.PlayState;
 
 public class VectorDefender implements Application {
 	
+	Input input;
+	Gamepad gamepad;
 	GameState gamestate;
 	
 	@Override
@@ -16,21 +19,27 @@ public class VectorDefender implements Application {
 		Resources.loadFont("fonts/imagine_16.fnt", "imagine12");
 		Resources.loadFont("fonts/imagine_16.fnt", "imagine16");
 		Resources.loadFont("fonts/Imagine.fnt", "imagine18");
-		Resources.loadFont("fonts/metroidPrime32.fnt", "TitleFont");
 		Resources.loadTexture("textures/Player.png", "player");
 		Resources.loadTexture("textures/bomb.png", "bomb");
 		Resources.loadTexture("textures/white.png", "blank");
-		Input.setMouseGrabbed(true);
+		Resources.loadTexture("textures/menuTitle.png", "title");
 		
-		gamestate = new GameState();
+		input = new Input();
+		input.setMouseGrabbed(true);
+		
+		gamepad = new Gamepad();
+		
+		gamestate = new GameState(input, gamepad);
 		gamestate.registerGState(new MenuState(gamestate));
 		gamestate.registerGState(new PlayState(gamestate));
+		gamestate.initialize();
 	}
 
 	@Override
 	public void update() {
 		gamestate.update();
-		Input.update();
+		input.poll();
+		gamepad.poll();
 	}
 	
 	@Override
