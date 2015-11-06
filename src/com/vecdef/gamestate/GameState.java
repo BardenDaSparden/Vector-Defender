f14466e698ff97328f34d9e6701bd37c713cf9ad
Branden Monroe <brandencmonroe@gmail.com>
2015-11-05 21:05:14 -0600

    Configured for external input dependencies.

--- src/com/vecdef/gamestate/GameState.java
+++ src/com/vecdef/gamestate/GameState.java
@@ -5,17 +5,35 @@ import java.util.ArrayList;
 import org.javatroid.core.Input;
 
 import com.toolkit.inputstate.Gamepad;
+import com.vecdef.analyze.AudioAnalyzer;
+import com.vecdef.audio.MusicPlayer;
+import com.vecdef.core.MinimFileHandler;
+
+import ddf.minim.Minim;
 
 public class GameState {
 
+	protected Minim minim;
+	protected MusicPlayer player;
+	protected AudioAnalyzer analyzer;
 	protected Input input;
 	protected Gamepad gamepad;
+	
 	ArrayList<GState> allStates;
 	int idx;
 	
-	public GameState(Input input, Gamepad gamepad){
-		this.input = input;
-		this.gamepad = gamepad;
+	public GameState(){
+		MinimFileHandler fHandler = new MinimFileHandler();
+		minim = new Minim(fHandler);
+		
+		player = new MusicPlayer(minim);
+		analyzer = new AudioAnalyzer(player);
+		
+		input = new Input();
+		input.setMouseGrabbed(true);
+		
+		gamepad = new Gamepad();
+		
 		allStates = new ArrayList<GState>();
 		idx = 0;
 	}
@@ -25,6 +43,18 @@ public class GameState {
 	}
 	
 	public void update(){
+		if(gamepad.isButtonPressed(Gamepad.DPAD_RIGHT_BUTTON)){
+			player.nextTrack();
+		}
+		
+		if(gamepad.isButtonPressed(Gamepad.DPAD_LEFT_BUTTON)){
+			player.previousTrack();
+		}
+		
+		input.poll();
+		gamepad.poll();
+		player.poll();
+		
 		allStates.get(idx).update();
 	}
 	
@@ -35,6 +65,7 @@ public class GameState {
 	public void destroy(){
 		GState state = allStates.get(idx);
 		state.destroy();
+		player.destroy();
 	}
 	
 	public void next(){
