096eed2127eff65722b0e83d9a6440e2a29ee0b4
Branden Monroe <brandencmonroe@gmail.com>
2015-11-05 21:10:16 -0600

    Added high-score support from external file stored in Jar.

--- src/com/vecdef/objects/PlayerStats.java
+++ src/com/vecdef/objects/PlayerStats.java
@@ -1,21 +1,94 @@
 package com.vecdef.objects;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
 public class PlayerStats{
 	
-	private int score = 0;
+	final String HIGHSCORE_INPUT_PATH = "data/highscore";
+	final String HIGHSCORE_OUTPUT_PATH = "resources/data/highscore";
+	
+	public static final int MAX_LIVES = 8;
+	public static final int MAX_ENERGY = 4000;
+	
+	private long score = 0;
 	private int multiplier = 1;
 	private int lives = 3;
+	private int energy = 0;
 	private int bombs = 4;
-
+	private long highscore;
+	
 	public void reset(){
 		score = 0;
 		multiplier = 1;
-		lives = 3;
+		lives = 4;
+		energy = 0;
 		bombs = 3;
+		highscore = loadHighscore();
 	}
 
+	public long loadHighscore(){
+		long hs = -1;
+		InputStream stream = ClassLoader.getSystemResourceAsStream(HIGHSCORE_INPUT_PATH);
+		if(stream != null){
+			Scanner scanner = new Scanner(stream);
+			hs = Long.parseLong(scanner.nextLine().trim());
+			scanner.close();
+		}
+		return hs;
+	}
+	
+	public void writeHighscore(){
+		try {
+			File file = new File(HIGHSCORE_OUTPUT_PATH);
+			if(!file.exists()){
+				file.createNewFile();
+			}
+			OutputStream stream = new FileOutputStream(file);
+			OutputStreamWriter writer = new OutputStreamWriter(stream, "UTF-8");
+			writer.write(highscore+"");
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			System.err.println("\nWARNING: Unable to write to " + HIGHSCORE_OUTPUT_PATH);
+			e.printStackTrace();
+		}
+	}
+	
+	public long getHighscore(){
+		return highscore;
+	}
+	
 	public void addScore(int s){
 		score += (s * multiplier);
+		if(score > highscore)
+			highscore = score;
+	}
+	
+	public void addEnergy(int e){
+		energy += e;
+		if(energy > MAX_ENERGY)
+			energy = MAX_ENERGY;
+	}
+	
+	public boolean hasEnergy(int e){
+		return (energy - e) > 0;
+	}
+	
+	public boolean useEnergy(int e){
+		boolean b = hasEnergy(e);
+		if(b)
+			energy -= e;
+		return b;
+	}
+	
+	public int getEnergy(){
+		return energy;
 	}
 
 	public void increaseMultiplier(){
@@ -34,7 +107,7 @@ public class PlayerStats{
 		bombs--;
 	}
 
-	public int getScore(){
+	public long getScore(){
 		return score;
 	}
 
