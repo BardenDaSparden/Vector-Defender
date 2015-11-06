8e13729cf98045bdd9481e28e9c16b093bd37ddc
Branden Monroe <brandencmonroe@gmail.com>
2015-11-05 21:08:20 -0600

    Recode of MainMenu state.

--- src/com/vecdef/gamestate/MenuState.java
+++ src/com/vecdef/gamestate/MenuState.java
@@ -1,23 +1,15 @@
 package com.vecdef.gamestate;
 
-import javax.sound.sampled.AudioFormat;
-
 import org.javatroid.core.Resources;
 import org.javatroid.graphics.BlendState;
 import org.javatroid.graphics.Color;
 import org.javatroid.graphics.OrthogonalCamera;
 import org.javatroid.graphics.SpriteBatch;
-import org.javatroid.graphics.Texture;
 import org.javatroid.text.BitmapFont;
 import org.lwjgl.opengl.Display;
 
 import com.toolkit.inputstate.Gamepad;
-import com.vecdef.analyze.AudioAnalyzer;
-import com.vecdef.core.MinimFileHandler;
-
-import ddf.minim.AudioListener;
-import ddf.minim.AudioPlayer;
-import ddf.minim.Minim;
+import com.vecdef.core.ApplicationLauncher;
 
 public class MenuState extends GState{
 
@@ -25,82 +17,67 @@ public class MenuState extends GState{
 		super(gamestate);
 	}
 
-	MinimFileHandler fHandler;
-	Minim minim;
-	AudioPlayer player;
-	AudioAnalyzer analyzer;
-	
-	int sampleRate;
-	int bufferSize;
-	float[] dataStream;
-
-	SpriteBatch batch;
-	OrthogonalCamera camera;
-	
-	Texture title;
+	Renderer renderer;
+	OrthogonalCamera camera;	
 	BitmapFont font;
-	float time;
-	Color color;
+	String[] menuItems = {"Start Endless Mode", "Exit"};
+	Color itemColor;
+	int menuItemIdx = 0;
 	
 	@Override
 	public void initialize() {
-		fHandler = new MinimFileHandler();
-		minim = new Minim(fHandler);
-		player = minim.loadFile("astro.wav");
-		AudioFormat format = player.getFormat();
-		sampleRate = (int) format.getSampleRate();
-		bufferSize = player.bufferSize();
-		dataStream = new float[bufferSize];
-		
-		player.addListener(new AudioListener() {
-			
-			@Override
-			public void samples(float[] sampL, float[] sampR) {
-				dataStream = sampL;
-			}
-			
-			@Override
-			public void samples(float[] samp) {
-				dataStream = samp;
-			}
-		});
-		
-		player.loop();
-		
-		analyzer = new AudioAnalyzer(bufferSize, sampleRate);
-		
-		batch = new SpriteBatch(1000);
+		renderer = new Renderer();
 		camera = new OrthogonalCamera((int)Display.getWidth(), (int)Display.getHeight());
-		batch.setCamera(camera);
-		
-		title = Resources.getTexture("title");
+		renderer.setCamera(camera);
 		font = Resources.getFont("imagine12");
-		color = new Color(1, 1, 1, 1);
+		itemColor = new Color(0.5f, 0.5f, 0.5f, 1);
 	}
 
 	@Override
 	public void update() {
-		time++;
-		color.w = (float)Math.abs(Math.sin(time * 0.1f));
-		if(gamestate.gamepad.isButtonPressed(Gamepad.START_BUTTON)){
-			gamestate.next();
+		if(gamestate.gamepad.isButtonPressed(Gamepad.DPAD_DOWN_BUTTON)){
+			menuItemIdx = (menuItemIdx + 1) % menuItems.length;
+		}
+		
+		if(gamestate.gamepad.isButtonPressed(Gamepad.DPAD_UP_BUTTON)){
+			if(menuItemIdx == 0)
+				menuItemIdx = menuItems.length - 1;
+			else
+				menuItemIdx--;
+		}
+		
+		if(gamestate.gamepad.isButtonPressed(Gamepad.A_BUTTON) || gamestate.gamepad.isButtonPressed(Gamepad.START_BUTTON)){
+			if(menuItemIdx == 0){
+				gamestate.next();
+			} else if(menuItemIdx == 1){
+				ApplicationLauncher.exit();
+			}
 		}
 	}
 
 	@Override
 	public void draw() {
-		analyzer.drawLogFreqSpectrumAvg(dataStream, batch);
-		batch.begin(BlendState.ADDITIVE);
-			batch.draw(0, 40, title.getWidth(), title.getHeight(), 0, title);
+		SpriteBatch batch = renderer.SpriteBatch();
+		batch.begin(BlendState.ALPHA);
+			int n = menuItems.length;
+			int y = 15;
+			for(int i = 0; i < n; i++){
+				if(menuItemIdx == i)
+					itemColor.set(1, 1, 1, 1);
+				else
+					itemColor.set(0.5f, 0.5f, 0.5f, 1);
+				String item = menuItems[i];
+				batch.setColor(itemColor);
+				font.drawStringCentered(0, y, item, batch);
+				y -= 30;
+			}
 			
-			batch.setColor(1, 1, 1, color.w);
-			font.drawStringCentered(0, -20, "Press Start to Begin...", batch);
-			batch.setColor(1, 1, 1, 1);
 		batch.end();
+		batch.setColor(1, 1, 1, 1);
 	}
 
 	@Override
 	public void destroy() {
-		player.close();
+		
 	}
 }
