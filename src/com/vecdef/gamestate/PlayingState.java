f61038859cf26d689ed3dab43d61d8782677523b
Branden Monroe <brandencmonroe@gmail.com>
2016-09-30 18:01:07 -0500

    "HUDController" refactor, now HUD and HUDRenderer

--- src/com/vecdef/gamestate/PlayingState.java
+++ src/com/vecdef/gamestate/PlayingState.java
@@ -13,6 +13,8 @@ import org.javatroid.math.Vector2f;
 import com.vecdef.objects.HUD;
 import com.vecdef.objects.Player;
 import com.vecdef.objects.Scene;
+import com.vecdef.rendering.HUDRenderer;
+import com.vecdef.rendering.Renderer;
 import com.vecdef.rendering.SceneRenderer;
 
 public class PlayingState extends GameState {
@@ -22,11 +24,13 @@ public class PlayingState extends GameState {
 	}
 	
 	OrthogonalCamera camera;
-	OrthogonalCamera hudCamera;
-	SceneRenderer sceneRenderer;
+	OrthogonalCamera overlayCamera;
 	
 	Scene scene;
-	HUD hudController;
+	SceneRenderer sceneRenderer;
+	
+	HUD hud;
+	HUDRenderer hudRenderer;
 	
 	CubicInterpolator interpolator = new CubicInterpolator(new Vector2f(0.35f, 0.0f), new Vector2f(0.75f, 1.0f));
 	Texture white;
@@ -35,11 +39,17 @@ public class PlayingState extends GameState {
 	
 	@Override
 	public void initialize() {
-		camera = new OrthogonalCamera(instance.settings.width, instance.settings.height);
-		hudCamera = new OrthogonalCamera(instance.settings.width, instance.settings.height);
-	    scene = new Scene(instance.settings.width, instance.settings.height, instance.input);
-	    sceneRenderer = new SceneRenderer(instance.settings.width, instance.settings.height, scene, instance.renderer);
-	    hudController = new HUD(scene, instance.renderer, instance.settings.width, instance.settings.height);
+		
+		float width = instance.settings.width;
+		float height = instance.settings.height;
+		Renderer renderer = instance.renderer;
+		
+		camera = new OrthogonalCamera(width, height);
+		overlayCamera = new OrthogonalCamera(width, height);
+	    scene = new Scene((int)width, (int)height, instance.input);
+	    sceneRenderer = new SceneRenderer((int)width, (int)height, scene, instance.renderer);
+	    hud = new HUD(scene);
+	    hudRenderer = new HUDRenderer((int)width, (int)height, renderer);
 	    white = Resources.getTexture("blank");
 	    fadeInTimer = new Timer(FADE_IN_TIME);
 	    fadeInTimer.setCallback(new TimerCallback() {
@@ -95,27 +105,27 @@ public class PlayingState extends GameState {
 			x /= (float)playerCount;
 			y /= (float)playerCount;
 			
-			float camX = interpolator.interpolate(cameraPosition.x, x, 0.05f);
-			float camY = interpolator.interpolate(cameraPosition.y, y, 0.05f);
+			float camX = interpolator.interpolate(cameraPosition.x, x, 0.10f);
+			float camY = interpolator.interpolate(cameraPosition.y, y, 0.10f);
 			camera.getTranslation().set(camX, camY);
 			
 		} else {
 			Vector2f cameraPosition = camera.getTranslation();
 			Vector2f playerPosition = scene.getPlayer().getTransform().getTranslation();
-			float newCameraX = interpolator.interpolate(cameraPosition.x, playerPosition.x, 0.05f);
-			float newCameraY = interpolator.interpolate(cameraPosition.y, playerPosition.y, 0.05f);
+			float newCameraX = interpolator.interpolate(cameraPosition.x, playerPosition.x, 0.10f);
+			float newCameraY = interpolator.interpolate(cameraPosition.y, playerPosition.y, 0.10f);
 			camera.getTranslation().set(newCameraX, newCameraY);
 		}
 		camera.update();
 		
 		scene.update();
 		
-		int liveCount = scene.getPlayer().getStats().getLiveCount();
-		if(liveCount < 0){
-			scene.getPlayer().getStats().writeHighscore();
-			scene.reset();
-			instance.setGameState(GameStates.MAIN_MENU);
-		}
+//		int liveCount = scene.getPlayer().getStats().getLiveCount();
+//		if(liveCount <= 0){
+//			scene.getPlayer().getStats().writeHighscore();
+//			scene.reset();
+//			instance.setGameState(GameStates.MAIN_MENU);
+//		}
 		
 		fadeInTimer.tick();
 	}
@@ -124,11 +134,11 @@ public class PlayingState extends GameState {
 	public void render() {
 		instance.renderer.setCamera(camera);
 	    sceneRenderer.draw();
-	    hudController.draw();
+	    hudRenderer.draw(hud);
 	    
 	    float opacity = 1.0f - fadeInTimer.percentComplete();
 	    SpriteBatch batch = instance.batch;
-	    batch.setCamera(hudCamera);
+	    batch.setCamera(overlayCamera);
 	    batch.setColor(0, 0, 0, opacity);
 	    batch.begin(BlendState.ALPHA);
 	    	batch.draw(0, 0, instance.settings.width, instance.settings.height, 0, white);
